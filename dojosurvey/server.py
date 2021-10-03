7351194190ad335c999cdc372f53635fa6b92777
Carson Lee <heesung199530@gmail.com>
2021-10-03 14:03:04 -0400

    Survey

---
+++ dojosurvey/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, session, redirect, request
+
+app = Flask(__name__)
+
+app.secret_key = "anything"
+
+@app.route ("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/process", methods=["POST"])
+def page():
+    session["name"] = request.form["name"]
+    session["location"] = request.form["location"]
+    session["language"] = request.form["language"]
+    session["comment"] = request.form["comment"]
+    return redirect("/result")
+
+@app.route("/result")
+def res():
+    return render_template("results.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
